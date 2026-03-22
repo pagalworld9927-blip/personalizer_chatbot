f0132d883057ac6775303299ffa0a1bd9f0ebbf7
Manish Singh <pagalworld9927@gmail.com>
2026-03-22 11:09:41 +0530

    file stucture and setup project

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from personalizer-chatbot!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/loggers.py
@@ -0,0 +1,15 @@
+import loggging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_dir = os.path.join(os.getcwd(), 'log')
+os.makedirs(log_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
+
+loggging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = loggging.INFO
+)
